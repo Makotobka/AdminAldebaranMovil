3a225236c4921745c3331b4eeeaac75110946661
Makotobka <makotobka@hotmail.com>
2018-10-16 23:08:52 -0500

    trabajo casa

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,7 @@
+import { DashboardGeneralPage } from './../dashboard-general/dashboard-general';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, Validators } from '@angular/forms';
 
 /**
  * Generated class for the LoginPage page.
@@ -15,11 +17,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  loginForm
+
+  constructor(public navCtrl: NavController, public fb: FormBuilder,public navParams: NavParams) {
+    let EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+    this.loginForm = fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.pattern(EMAIL_REGEXP)])],
+      password: ['', Validators.compose([Validators.minLength(4), Validators.required])]
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
+
+  clickConfiguracion(){
+
+  }
+
+  clickLogin(){
+    this.navCtrl.setRoot(DashboardGeneralPage);
+  }
+
+
 
 }
