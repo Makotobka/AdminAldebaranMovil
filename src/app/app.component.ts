44358d8a222246be92ebfa0805a7f095af9cf7ca
Makotobka <makotobka@hotmail.com>
2018-10-17 09:20:41 -0500

    cambios oficina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { CajaPage } from '../pages/caja/caja';
+import { DashboardEspecificoPage } from '../pages/dashboard-especifico/dashboard-especifico';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = CajaPage;
+  rootPage:any = DashboardEspecificoPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
